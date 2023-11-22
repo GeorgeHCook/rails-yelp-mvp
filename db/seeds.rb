17ee3cc3a26268697ecc11ec8534a48e5d294845
George Cook <georgehhcook@gmail.com>
2023-11-22 15:45:00 +0000

    index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning databse'
+Restaurant.destroy_all
+
+puts 'Creating Database'
+mayflower = { name: 'The Mayflower', address: 'Bristol Town Center', category: 'chinese', phone_number: '123' }
+tipo00 = { name: 'Tipo00', address: 'little bourke street', category: 'italian', phone_number: '234' }
+tomono = { name: 'Tomono Ramen', address: 'old market', category: 'japanese', phone_number: '345' }
+little_french = { name: 'LittleFrench', address: 'westbury', category: 'french', phone_number: '456' }
+strawberry_theif = { name: 'The Strawberry Theif', address: 'Bristol', category: 'belgian', phone_number: '567' }
+
+[mayflower, tipo00, tomono, little_french, strawberry_theif].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'finished!'
